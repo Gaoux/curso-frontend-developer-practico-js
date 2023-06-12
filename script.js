4394a9a9b326f01feca264964c1416233599e753
Gustavo Parra <gustavoparratanoux@gmail.com>
2023-06-12 18:04:55 -0500

    merge shopping cart menu to index

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const myOrder = document.querySelector('.product-detail');
 const mediaQuery = window.matchMedia('(min-width: 640px)');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleCartAside);
 
 menuDisplay(mediaQuery); //Initial check
 mediaQuery.addListener(menuDisplay); // Attach listener function on state changes
 
 document.onclick = function (e) {
   let targetClass = e.target.getAttribute('class');
-
   if (targetClass !== 'mobile-menu' && targetClass !== 'menu')
     mobileMenu.classList.add('inactive');
 
   if (targetClass !== 'desktop-menu' && targetClass !== 'navbar-email')
     desktopMenu.classList.add('inactive');
+
+  if (
+    targetClass !== 'navbar-shopping-cart__icon' &&
+    targetClass !== 'navbar-shopping-cart__number' &&
+    targetClass !== '.product-detail'
+  )
+    myOrder.classList.add('inactive');
 };
 
 function toggleDesktopMenu() {
@@ -27,6 +36,11 @@ function toggleDesktopMenu() {
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCartAside() {
+  myOrder.classList.toggle('inactive');
+}
+
 //Media querie to display menu
 function menuDisplay(mediaQuery) {
   if (mediaQuery.matches) mobileMenu.classList.add('inactive');
